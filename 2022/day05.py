597764663d6f7271fc381ee73122465bb72f38ea
Gerold Busch <gerold.busch@gmail.com>
2022-12-05 19:02:32 +0000

    day05, second part

--- 2022/day05.py
+++ 2022/day05.py
@@ -1,7 +1,15 @@
+from dataclasses import dataclass
 from typing import Dict, List
 from itertools import zip_longest
 from functools import reduce
 import re
+
+
+@dataclass
+class Move:
+    steps: int
+    origin: int
+    destination: int
 
 
 def parse_setup(setup_input: str) -> Dict[int, List[str]]:
@@ -27,47 +35,74 @@ def parse_setup(setup_input: str) -> Dict[int, List[str]]:
     }
 
 
-def parse_moves(move_line: str):
+def parse_moves(move_line: str) -> Move:
     """
     >>> parse_moves("move 2 from 9 to 8")
-    (2, 9, 8)
+    Move(steps=2, origin=9, destination=8)
     >>> parse_moves("move 13 from 4 to 2")
-    (13, 4, 2)
+    Move(steps=13, origin=4, destination=2)
     """
     matches = re.match(r"move (\d+) from (\d+) to (\d+)", move_line)
-    return tuple(int(matches.group(x)) for x in range(1, 4))
+    return Move(
+        steps=int(matches.group(1)),
+        origin=int(matches.group(2)),
+        destination=int(matches.group(3)),
+    )
 
 
-def make_move(crates, move):
+def make_move(crates: Dict[int, List[str]], move: Move) -> Dict[int, List[str]]:
     """
-    >>> make_move({1: ['Z', 'N'], 2: ['M', 'C', 'D'], 3: ['P']}, (2, 1))
+    >>> make_move({1: ['Z', 'N'], 2: ['M', 'C', 'D'], 3: ['P']}, Move(1, 2, 1))
     {1: ['Z', 'N', 'D'], 2: ['M', 'C'], 3: ['P']}
     """
-    crates[move[1]].append(crates[move[0]].pop())
+    crates[move.destination].append(crates[move.origin].pop())
     return crates
 
 
-def make_moves(crates, move):
+def make_moves(crates: Dict[int, List[str]], move: Move) -> Dict[int, List[str]]:
     """
-    >>> make_moves({1: ['Z', 'N', 'D'], 2: ['M', 'C'], 3: ['P']}, (3, 1, 3))
+    >>> make_moves({1: ['Z', 'N', 'D'], 2: ['M', 'C'], 3: ['P']}, Move(3, 1, 3))
     {1: [], 2: ['M', 'C'], 3: ['P', 'D', 'N', 'Z']}
     """
-    return reduce(lambda x, _: make_move(x, (move[1], move[2])), range(move[0]), crates)
+    return reduce(lambda x, _: make_move(x, move), range(move.steps), crates)
 
 
-def part1(input_setup, moves):
+def make_9001_move(crates: Dict[int, List[str]], move: Move) -> Dict[int, List[str]]:
     """
-    >>> part1({1: ['Z', 'N'], 2: ['M', 'C', 'D'], 3: ['P']}, [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)])
+    >>> make_9001_move({1: ['Z', 'N', 'D'], 2: ['M', 'C'], 3: ['P']}, Move(3, 1, 3))
+    {1: [], 2: ['M', 'C'], 3: ['P', 'Z', 'N', 'D']}
+    """
+    crates[move.destination] += crates[move.origin][-move.steps :]
+    crates[move.origin] = crates[move.origin][: -move.steps]
+    return crates
+
+
+def part1(input_setup: Dict[int, List[str]], moves: Move) -> str:
+    """
+    >>> part1({1: ['Z', 'N'], 2: ['M', 'C', 'D'], 3: ['P']}, [Move(1, 2, 1), Move(3, 1, 3), Move(2, 2, 1), Move(1, 1, 2)])
     'CMZ'
     """
     final_setup = reduce(lambda x, move: make_moves(x, move), moves, input_setup)
-    return "".join(final_setup[i].pop() for i in range(1, max(final_setup.keys())+1))
+    return "".join(final_setup[i].pop() for i in range(1, max(final_setup.keys()) + 1))
+
+
+def part2(input_setup: Dict[int, List[str]], moves: Move) -> str:
+    """
+    >>> part2({1: ['Z', 'N'], 2: ['M', 'C', 'D'], 3: ['P']}, [Move(1, 2, 1), Move(3, 1, 3), Move(2, 2, 1), Move(1, 1, 2)])
+    'MCD'
+    """
+    final_setup = reduce(lambda x, move: make_9001_move(x, move), moves, input_setup)
+    return "".join(
+        final_setup[i].pop()
+        for i in range(1, max(final_setup.keys()) + 1)
+        if final_setup[i]
+    )
 
 
 if __name__ == "__main__":
     with open("data/day05.txt", "r") as f:
         inp_str = f.read()
-    
+
     split_input = inp_str.split("\n\n")
     stacks = parse_setup(split_input[0])
     moves = list(map(parse_moves, split_input[1].splitlines()))
@@ -75,3 +110,7 @@ if __name__ == "__main__":
     print("part1")
     print(part1(stacks, moves))
 
+    stacks = parse_setup(split_input[0])
+
+    print("part2")
+    print(part2(stacks, moves))
